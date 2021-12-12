f49c2100cfe68e08ccef22fcb12c752ee4cfab9e
yosmont <yann.osmont@epitech.eu>
2021-12-12 17:50:45 +0100

    UserView - you can unfollow now

--- Gitech/src/components/UserView/FollowButton/index.js
+++ Gitech/src/components/UserView/FollowButton/index.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import FullWidthCustomButton from "@components/FullWidthCustomButton";
 
-function followUser(octokit, username) {
-    octokit.rest.users.follow({ username }).then((value) => {
-        alert(username + " is followed");
-    }).catch((err) => {
-        alert(err);
-    });
+function followUser(octokit, username, followStatus) {
+    if (followStatus) {
+        octokit.rest.users.unfollow({ username: username }).then((value) => {
+            alert(username + " is unfollowed");
+        }).catch((err) => {
+            alert("unfollow : " + err);
+        });
+        return false;
+    } else {
+        octokit.rest.users.follow({ username: username }).then((value) => {
+            alert(username + " is followed");
+        }).catch((err) => {
+            alert("follow : " + err);
+        });
+        return true;
+    }
 }
 
 const FollowButton = (props) => {
-    var buttonText = "Follow " + props.username;
+    const followText = "Follow " + props.username;
+    const unfollowText = "Unfollow " + props.username;
+    const [followStatus, onFollowStatusChange] = React.useState(undefined);
+    useEffect(() => {
+        props.octokit.rest.users.checkPersonIsFollowedByAuthenticated({ username: props.username }).then((value) => {
+            onFollowStatusChange(true)
+        }).catch((err) => {
+            onFollowStatusChange(false);
+        });
+    });
     return (
-        <FullWidthCustomButton onPress={() => followUser(props.octokit, props.username)} Text={buttonText} />
+        <FullWidthCustomButton onPress={() => onFollowStatusChange(followUser(props.octokit, props.username, followStatus))} Text={followStatus ? unfollowText : followText} />
     );
 }
 
