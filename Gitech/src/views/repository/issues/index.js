3907369d6796662398d67478af51d942a781104e
Héléna Ganeman-Valot <helena.ganeman-valot@epitech.eu>
2021-12-12 20:51:40 +0100

    Added create issue

--- Gitech/src/views/repository/issues/index.js
+++ Gitech/src/views/repository/issues/index.js
@@ -1,16 +1,56 @@
 import React from 'react';
-import {Pressable} from "react-native";
-import {styles, Flex, Text, Wrapper, StatusIcon} from "./styles";
+import {View, Pressable, Modal, TextInput} from "react-native";
+import {styles, stylesActive, Flex, Text, Wrapper, StatusIcon} from "./styles";
 import ButtonWithIcon from "@src/components/ButtonWithIcon";
 import {FontAwesome5, Ionicons} from "@expo/vector-icons";
 
 const Issues = (props) => {
+  const [modalVisible, setModalVisible] = React.useState(false);
+  const [newIssueTitle, setNewIssueTitle] = React.useState("");
+  const [newIssueBody, setNewIssueBody] = React.useState("");
+
   console.log(props.route.params.issues);
   return (
     <Wrapper>
+      <Modal
+      animationType="fade"
+      transparent={true}
+      visible={modalVisible}
+      onRequestClose={() => setModalVisible(!modalVisible)}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <TextInput
+              placeholder={'Title'}
+              placeholderTextColor={'white'}
+              style={styles.textInput}
+              onChangeText={(value) => setNewIssueTitle(value)}
+            />
+            <TextInput
+              placeholder={'Leave a comment'}
+              placeholderTextColor={'white'}
+              style={styles.textInput}
+              multiline={true}
+              numberOfLines={4}
+              onChangeText={(value) => setNewIssueBody(value)}
+            />
+            <Pressable
+              style={stylesActive(newIssueTitle !== "").button}
+              onPress={() => createIssue(
+                props.route.params.octokit,
+                props.route.params.repo,
+                newIssueTitle,
+                newIssueBody
+              )}>
+              <Text style={stylesActive(newIssueTitle !== "").text}>Submit new issue</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+
       <Pressable
         style={styles.button}
-        onPress={() => createIssue(props.route.params.navigation, props.route.params.octokit)}>
+        onPress={() => setModalVisible(true)/*createIssue(props.route.params.navigation, props.route.params.octokit)*/}>
         <Text>New issue</Text>
       </Pressable>
       {
@@ -41,8 +81,22 @@ const getIssue = (navigation, octokit, repo, issue) => {
   navigation.push('Issue', {navigation: navigation, octokit: octokit, repo: repo, issue: issue})
 }
 
-const createIssue = (navigation, octokit) => {
-  navigation.push('CreateIssue', {navigation: navigation, octokit: octokit})
+const createIssue = (octokit, repo, title, body) => {
+  if (title !== "") {
+    octokit.rest.issues.create(body !== "" ?
+      {
+        owner: repo.owner.login,
+        repo: repo.name,
+        title: title,
+        body: body
+      } : {
+        owner: repo.owner.login,
+        repo: repo.name,
+        title: title
+      }).then(() => {
+
+    })
+  }
 }
 
 export default Issues;
--- Gitech/src/views/repository/issues/styles.js
+++ Gitech/src/views/repository/issues/styles.js
@@ -21,7 +21,40 @@ export const StatusIcon = styled.View`
   padding-right: 5px;
 `;
 
+export const centeredView = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  margin-top: 22px;
+`;
+
+export const ModalCard = styled.View`
+  align-self: center;
+  background-color: ${colors.card};
+  border-width: 1px;
+  border-color: #242c35;
+  border-radius: 3px;
+  padding: 2px;
+  margin: 20px;
+  width: 300px;
+`;
+
 export const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: colors.card,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: colors.buttonBorder,
+    padding: 35,
+    alignItems: "center"
+  },
   button: {
     alignItems: "center",
     backgroundColor: '#238636',
@@ -32,5 +65,30 @@ export const styles = StyleSheet.create({
     width: 100,
     marginTop: 10,
     marginBottom: 10
+  },
+  textInput: {
+    backgroundColor: 'black',
+    borderRadius: 6,
+    lineHeight: 20,
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingLeft: 12,
+    paddingRight: 12,
+    marginBottom: 10,
+    color: 'white',
+    width: 300
+  }
+});
+
+export const stylesActive = (active) => StyleSheet.create({
+  button: {
+    alignItems: "center",
+    backgroundColor: active ? '#238636' : '#1d5a2d',
+    padding: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#238636',
+    width: 150,
+    marginTop: 10
   }
 });
